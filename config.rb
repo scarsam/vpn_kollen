589dc281e528be9dd78f6095dd56085a149d34da
Sam Ojling <s.ojling@gmail.com>
2017-10-16 22:51:41 -0700

    Added bulma css framework, created new layout

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "reviews/*", :layout => 'review.layout'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
